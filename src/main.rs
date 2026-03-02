f6f6c0502db0d86c026e3814b3ec28f1ab1ce09f
bolli24 <9805065+bolli24@users.noreply.github.com>
2026-03-02 17:11:23 +0100

    Basic modification

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,12 @@ use mozjpeg_sys::*;
 use anyhow::{Context, bail};
 use sha256::digest;
 
+use crate::zigzag::ZigZagExt;
+
+pub mod zigzag;
+
 const INPUT_PATH: &str = "./test/CRW_2614_(Elsterflutbecken).jpg";
-const OUTPUT_PATH: &str = "output.jpg";
+const OUTPUT_PATH: &str = "./test/output.jpg";
 
 fn main() -> anyhow::Result<()> {
 	let mut file = File::open(INPUT_PATH).context("Error opening input file.")?;
@@ -20,10 +24,10 @@ fn main() -> anyhow::Result<()> {
 
 	println!("Read '{}': {}KiB", INPUT_PATH, content.len() / 1024);
 	println!("SHA256: {}", digest(&content));
+	let capacity = get_capacity(&content).context("Error getting capacity.")?;
+	println!("Capacity: {} KiB", capacity / 1024);
 
-	let no_op = |_: Block| {};
-
-	let modified_content = unsafe { process_jpeg_blocks(&content, no_op) }?;
+	let modified_content = add_one(&content)?;
 
 	let mut modified = File::create(OUTPUT_PATH).context("Error creating output file.")?;
 	modified
@@ -36,12 +40,53 @@ fn main() -> anyhow::Result<()> {
 	Ok(())
 }
 
-pub struct Block<'a> {
-	component_index: usize,
-	row: usize,
-	column: usize,
-	data: &'a mut [i16; 64],
+pub fn add_one(jpeg_data: &[u8]) -> anyhow::Result<Vec<u8>> {
+	let mut modified = 0;
+
+	let add = |_: Block, coeffs: &mut BlockData| {
+		for c in coeffs.zigzag_mut().skip(5) {
+			if *c != -1 && *c != 0 && *c != 1 {
+				*c = c.saturating_add(1);
+				modified += 1;
+			}
+		}
+	};
+
+	let result = unsafe { process_jpeg_blocks(jpeg_data, add) };
+
+	println!("Modified {modified} bits");
+	result
 }
+
+pub fn get_capacity(jpeg_data: &[u8]) -> anyhow::Result<usize> {
+	Ok(get_component_capacity(jpeg_data)?.iter().sum())
+}
+
+pub fn get_component_capacity(jpeg_data: &[u8]) -> anyhow::Result<[usize; 3]> {
+	let mut capacity = [0, 0, 0];
+
+	let non_null_coeefs = |block: Block, coeffs: &BlockData| {
+		for c in coeffs.zigzag().skip(5) {
+			if *c != -1 && *c != 0 && *c != 1 {
+				capacity[block.component_index] += 1;
+			}
+		}
+	};
+
+	unsafe {
+		read_jpeg_blocks(jpeg_data, non_null_coeefs)?;
+	}
+
+	Ok([capacity[0] / 8, capacity[1] / 8, capacity[2] / 8])
+}
+
+pub struct Block {
+	pub component_index: usize,
+	pub row: usize,
+	pub column: usize,
+}
+
+pub type BlockData = [i16; 64];
 
 // JMSG_LENGTH_MAX is typically 200 in libjpeg
 const JMSG_LENGTH_MAX: usize = 200;
@@ -65,110 +110,10 @@ extern "C-unwind" fn custom_error_exit(cinfo: &mut jpeg_common_struct) {
 	}
 }
 
-pub unsafe fn process_jpeg_blocks<F>(jpeg_data: &[u8], mut process_block: F) -> anyhow::Result<Vec<u8>>
-where
-	F: FnMut(Block),
-{
-	let result = panic::catch_unwind(panic::AssertUnwindSafe(|| {
-		let mut err: jpeg_error_mgr = std::mem::zeroed();
-		jpeg_std_error(&mut err);
-
-		// 1. Override the fatal error exit function
-		err.error_exit = Some(custom_error_exit);
-
-		// 2. Setup Source (Decompressor)
-		let mut srcinfo: jpeg_decompress_struct = std::mem::zeroed();
-		srcinfo.common.err = &mut err;
-		jpeg_create_decompress(&mut srcinfo);
-
-		jpeg_mem_src(&mut srcinfo, jpeg_data.as_ptr(), jpeg_data.len() as c_ulong);
-
-		// Save the COM (Comment) marker
-		jpeg_save_markers(&mut srcinfo, 0xFE, 0xFFFF);
-		// Save all APP0 through APP15 markers (0xE0 to 0xEF)
-		for m in 0..16 {
-			jpeg_save_markers(&mut srcinfo, 0xE0 + m, 0xFFFF);
-		}
-
-		jpeg_read_header(&mut srcinfo, 1);
-
-		// Read coefficients into virtual arrays
-		let coef_arrays = jpeg_read_coefficients(&mut srcinfo);
-
-		// 3. Setup Destination (Compressor)
-		let mut dstinfo: jpeg_compress_struct = std::mem::zeroed();
-
-		dstinfo.common.err = &mut err;
-		jpeg_create_compress(&mut dstinfo);
-
-		// Setup output buffer for the new JPEG
-		let mut out_buffer: *mut c_uchar = ptr::null_mut();
-		let mut out_size: c_ulong = 0;
-
-		jpeg_mem_dest(&mut dstinfo, &mut out_buffer, &mut out_size);
-
-		// Copy tables and parameters to prevent re-quantization
-		jpeg_copy_critical_parameters(&srcinfo, &mut dstinfo);
-
-		// Start the compressor and link the arrays.
-		// This writes the SOI marker and readies the writing of data.
-		jpeg_write_coefficients(&mut dstinfo, coef_arrays);
-
-		// Walk the linked list of saved markers and write them to the compressor
-		// in order to preserve the input image's metadata
-		let mut marker = srcinfo.marker_list;
-		while !marker.is_null() {
-			jpeg_write_marker(
-				&mut dstinfo,
-				(*marker).marker as libc::c_int,
-				(*marker).data,
-				(*marker).data_length,
-			);
-			marker = (*marker).next;
-		}
-
-		// 3. Modify the Coefficients
-		for comp_idx in 0..srcinfo.num_components {
-			let comp_info = srcinfo.comp_info.add(comp_idx as usize);
-			let height_in_blocks = (*comp_info).height_in_blocks;
-			let width_in_blocks = (*comp_info).width_in_blocks;
-			let comp_coef_array = *coef_arrays.add(comp_idx as usize);
-
-			for row in 0..height_in_blocks {
-				// Pass 1 (TRUE) to request WRITE access to the virtual array
-				let block_row =
-					(*srcinfo.common.mem).access_virt_barray.unwrap()(&mut srcinfo.common, comp_coef_array, row, 1, 1);
-
-				for col in 0..width_in_blocks {
-					let block_ptr = (*block_row).add(col as usize);
-					// Yield the block and its coordinates to the provided closure
-					process_block(Block {
-						component_index: comp_idx as usize,
-						row: row as usize,
-						column: col as usize,
-						data: &mut *block_ptr,
-					});
-				}
-			}
-		}
-
-		// 4. Finish pipelines
-		jpeg_finish_compress(&mut dstinfo);
-		jpeg_destroy_compress(&mut dstinfo);
-		jpeg_finish_decompress(&mut srcinfo);
-		jpeg_destroy_decompress(&mut srcinfo);
-
-		// 5. Copy the C-allocated buffer to a Rust Vec, then free the C buffer
-		let result_vec = std::slice::from_raw_parts(out_buffer, out_size as usize).to_vec();
-		free(out_buffer as *mut c_void);
-
-		result_vec
-	}));
-
+fn handle_jpeg_panic<T>(result: std::thread::Result<T>) -> anyhow::Result<T> {
 	match result {
-		Ok(vec) => Ok(vec),
+		Ok(value) => Ok(value),
 		Err(err) => {
-			// Downcast the panic payload to extract the string we sent from custom_error_exit
 			if let Some(msg) = err.downcast_ref::<String>() {
 				bail!(msg.clone());
 			} else if let Some(msg) = err.downcast_ref::<&str>() {
@@ -178,4 +123,168 @@ where
 			}
 		}
 	}
+}
+
+unsafe fn with_decompressor<T, C, F>(jpeg_data: &[u8], err: &mut jpeg_error_mgr, mut configure: C, mut body: F) -> T
+where
+	C: FnMut(&mut jpeg_decompress_struct),
+	F: FnMut(&mut jpeg_decompress_struct, *mut *mut jvirt_barray_control) -> T,
+{
+	let mut srcinfo: jpeg_decompress_struct = std::mem::zeroed();
+	srcinfo.common.err = err;
+	jpeg_create_decompress(&mut srcinfo);
+
+	jpeg_mem_src(&mut srcinfo, jpeg_data.as_ptr(), jpeg_data.len() as c_ulong);
+	configure(&mut srcinfo);
+	jpeg_read_header(&mut srcinfo, 1);
+
+	let coef_arrays = jpeg_read_coefficients(&mut srcinfo);
+	let output = body(&mut srcinfo, coef_arrays);
+
+	jpeg_finish_decompress(&mut srcinfo);
+	jpeg_destroy_decompress(&mut srcinfo);
+
+	output
+}
+
+unsafe fn for_each_block_ptr<F>(
+	srcinfo: &mut jpeg_decompress_struct,
+	coef_arrays: *mut *mut jvirt_barray_control,
+	writable: bool,
+	mut visit: F,
+) where
+	F: FnMut(Block, *mut BlockData),
+{
+	for comp_idx in 0..srcinfo.num_components {
+		let comp_info = srcinfo.comp_info.add(comp_idx as usize);
+		let height_in_blocks = (*comp_info).height_in_blocks;
+		let width_in_blocks = (*comp_info).width_in_blocks;
+		let comp_coef_array = *coef_arrays.add(comp_idx as usize);
+
+		for row in 0..height_in_blocks {
+			let block_row = (*srcinfo.common.mem).access_virt_barray.unwrap()(
+				&mut srcinfo.common,
+				comp_coef_array,
+				row,
+				1,
+				if writable { 1 } else { 0 },
+			);
+
+			for col in 0..width_in_blocks {
+				let block_ptr = (*block_row).add(col as usize);
+				visit(
+					Block {
+						component_index: comp_idx as usize,
+						row: row as usize,
+						column: col as usize,
+					},
+					block_ptr,
+				);
+			}
+		}
+	}
+}
+
+pub unsafe fn process_jpeg_blocks<F>(jpeg_data: &[u8], mut process_block: F) -> anyhow::Result<Vec<u8>>
+where
+	F: FnMut(Block, &mut BlockData),
+{
+	let result = panic::catch_unwind(panic::AssertUnwindSafe(|| {
+		let mut err: jpeg_error_mgr = std::mem::zeroed();
+		jpeg_std_error(&mut err);
+
+		// 1. Override the fatal error exit function
+		err.error_exit = Some(custom_error_exit);
+
+		with_decompressor(
+			jpeg_data,
+			&mut err,
+			|srcinfo| {
+				// Save the COM (Comment) marker
+				jpeg_save_markers(srcinfo, 0xFE, 0xFFFF);
+				// Save all APP0 through APP15 markers (0xE0 to 0xEF)
+				for m in 0..16 {
+					jpeg_save_markers(srcinfo, 0xE0 + m, 0xFFFF);
+				}
+			},
+			|srcinfo: &mut jpeg_decompress_struct, coef_arrays| {
+				// 3. Setup Destination (Compressor)
+				let mut dstinfo: jpeg_compress_struct = std::mem::zeroed();
+
+				dstinfo.common.err = srcinfo.common.err;
+				jpeg_create_compress(&mut dstinfo);
+
+				// Setup output buffer for the new JPEG
+				let mut out_buffer: *mut c_uchar = ptr::null_mut();
+				let mut out_size: c_ulong = 0;
+
+				jpeg_mem_dest(&mut dstinfo, &mut out_buffer, &mut out_size);
+
+				// Copy tables and parameters to prevent re-quantization
+				jpeg_copy_critical_parameters(srcinfo, &mut dstinfo);
+
+				// Start the compressor and link the arrays.
+				// This writes the SOI marker and readies the writing of data.
+				jpeg_write_coefficients(&mut dstinfo, coef_arrays);
+
+				// Walk the linked list of saved markers and write them to the compressor
+				// in order to preserve the input image's metadata
+				let mut marker = srcinfo.marker_list;
+				while !marker.is_null() {
+					jpeg_write_marker(
+						&mut dstinfo,
+						(*marker).marker as libc::c_int,
+						(*marker).data,
+						(*marker).data_length,
+					);
+					marker = (*marker).next;
+				}
+
+				for_each_block_ptr(srcinfo, coef_arrays, true, |block, block_ptr| {
+					process_block(block, &mut *block_ptr);
+				});
+
+				jpeg_finish_compress(&mut dstinfo);
+				jpeg_destroy_compress(&mut dstinfo);
+
+				// Copy the C-allocated buffer to a Rust Vec, then free the C buffer
+				let result_vec = std::slice::from_raw_parts(out_buffer, out_size as usize).to_vec();
+				free(out_buffer as *mut c_void);
+
+				result_vec
+			},
+		)
+	}));
+
+	handle_jpeg_panic(result)
+}
+
+pub unsafe fn read_jpeg_blocks<F>(jpeg_data: &[u8], mut process_block: F) -> anyhow::Result<()>
+where
+	F: FnMut(Block, &BlockData),
+{
+	let result = panic::catch_unwind(panic::AssertUnwindSafe(|| {
+		let mut err: jpeg_error_mgr = std::mem::zeroed();
+		jpeg_std_error(&mut err);
+
+		// 1. Override the fatal error exit function
+		err.error_exit = Some(custom_error_exit);
+
+		with_decompressor(
+			jpeg_data,
+			&mut err,
+			|_| {},
+			|srcinfo: &mut jpeg_decompress_struct, coef_arrays| {
+				if srcinfo.num_components != 3 {
+					panic!("Only 3-component JPEGs are supported, found {}", srcinfo.num_components);
+				}
+
+				for_each_block_ptr(srcinfo, coef_arrays, false, |block, block_ptr| {
+					process_block(block, &*block_ptr);
+				});
+			},
+		);
+	}));
+
+	handle_jpeg_panic(result)
 }
---
+++ src/zigzag.rs
@@ -0,0 +1,59 @@
+#[rustfmt::skip]
+const ZIGZAG_INDICES: [usize; 64] = [
+     0,  1,  8, 16,  9,  2,  3, 10,
+    17, 24, 32, 25, 18, 11,  4,  5,
+    12, 19, 26, 33, 40, 48, 41, 34,
+    27, 20, 13,  6,  7, 14, 21, 28,
+    35, 42, 49, 56, 57, 50, 43, 36,
+    29, 22, 15, 23, 30, 37, 44, 51,
+    58, 59, 52, 45, 38, 31, 39, 46,
+    53, 60, 61, 54, 47, 55, 62, 63,
+];
+
+pub struct ZigZagMut<'a> {
+	ptr: *mut i16,
+	pos: usize,
+	_marker: std::marker::PhantomData<&'a mut i16>,
+}
+
+impl<'a> Iterator for ZigZagMut<'a> {
+	type Item = &'a mut i16;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		if self.pos >= 64 {
+			return None;
+		}
+
+		let idx = ZIGZAG_INDICES[self.pos];
+
+		self.pos += 1;
+
+		// SAFETY: ZIGZAG_INDICES contains 64 unique, in-bounds indices.
+		// It is impossible to yield overlapping mutable references.
+		unsafe { Some(&mut *self.ptr.add(idx)) }
+	}
+}
+
+pub trait ZigZagExt {
+	fn zigzag(&self) -> impl Iterator<Item = &i16>;
+	fn into_zigzag(self) -> impl Iterator<Item = i16>;
+	fn zigzag_mut(&mut self) -> ZigZagMut<'_>;
+}
+
+impl ZigZagExt for [i16; 64] {
+	fn zigzag(&self) -> impl Iterator<Item = &i16> {
+		ZIGZAG_INDICES.iter().map(move |&i| &self[i])
+	}
+
+	fn into_zigzag(self) -> impl Iterator<Item = i16> {
+		ZIGZAG_INDICES.into_iter().map(move |i| self[i])
+	}
+
+	fn zigzag_mut(&mut self) -> ZigZagMut<'_> {
+		ZigZagMut {
+			ptr: self.as_mut_ptr(),
+			pos: 0,
+			_marker: std::marker::PhantomData,
+		}
+	}
+}
